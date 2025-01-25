7f342062f905a438fd6b754956dfc09e9b2d0a00
Mariam <kelanimaryam1234@gmail.com>
2025-01-25 06:47:04 +0100

    server created using existing js file and content served from the filepath

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 import http from "http";
+import fs from "fs/promises";
+import url from "url";
+import path from "path";
+
+//Get current path of the file to use
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 //const PORT = 8000;
 const PORT = process.env.PORT;
 
 //create the server
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   try {
-    if (req.method === 'GET'){
-      if (req.url ==='/'){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end('<h1>Homepage</h1>');
+    if (req.method === "GET") {
+      let filePath;
+      if (req.url === "/") {
+        filePath = path.join(__dirname, "public", "index.html");
+      } else if (req.url === "/about") {
+        filePath = path.join(__dirname, "public", "about.html");
+      } else {
+        throw new Error('Page not found')
       }
-      else if (req.url === '/about'){
-        res.writeHead(200, {'Content-Type': 'text/html'})
-        res.end('<h1>About page</h1>');
-      }
-      else {
-        res.writeHead(404, {'Content-Type': 'text/html'})
-        res.end('<h1>Not found</h1>');
-      }
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
+    } else {
+      throw new Error("Method not allowed");
     }
-    else {
-      throw new Error('Method not found');
-    }
-  } catch (error){
-    res.writeHead(500, {'Content-Type': 'text/plain'});
-    res.end('server error')
-  }
 
+  } catch (error) {
+    res.writeHead(500, { "Content-Type": "text/plain" });
+    res.end("server error");
+  }
 });
 
 //listen to the server created
