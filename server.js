3a2c2aeff64a314756134a39bd94a08430ea0428
Mariam <kelanimaryam1234@gmail.com>
2025-01-25 05:51:08 +0100

    content of server.js router set up appropriately

--- server.js
+++ server.js
@@ -4,19 +4,29 @@ const PORT = process.env.PORT;
 
 //create the server
 const server = http.createServer((req, res) => {
-  if (req.url === '/'){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end('<h1>Local homepage</h1>')
+  try {
+    if (req.method === 'GET'){
+      if (req.url ==='/'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('<h1>Homepage</h1>');
+      }
+      else if (req.url === '/about'){
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.end('<h1>About page</h1>');
+      }
+      else {
+        res.writeHead(404, {'Content-Type': 'text/html'})
+        res.end('<h1>Not found</h1>');
+      }
+    }
+    else {
+      throw new Error('Method not found');
+    }
+  } catch (error){
+    res.writeHead(500, {'Content-Type': 'text/plain'});
+    res.end('server error')
   }
-  else if (req.url === '/about'){
-    res.writeHead(200, {'Content-Type': 'text/html'})
-    res.end(200, {'Content-Type': 'text/html'})
-  }
-  else {
-    res.writeHead(404, 'text/html')
-    res.end(`<h1>Not found</h1>`)
 
-  }
 });
 
 //listen to the server created
