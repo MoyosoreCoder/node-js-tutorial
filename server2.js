52af5323e4f6c1c81e2d6bfcd386925072e42fee
Mariam <kelanimaryam1234@gmail.com>
2025-01-26 07:11:06 +0100

    a new file created server2.js which handled routes RESTFUL API

--- server2.js
+++ server2.js
@@ -1,6 +1,8 @@
 import { createServer } from "http";
 const PORT = process.env.PORT;
 
+
+//data information
 const users = [
   { id: 1, name: "Ann Bee" },
   { id: 2, name: "Mary Kay" },
@@ -9,14 +11,17 @@ const users = [
 
 //create server by importing createServer
 const server = createServer((req, res) => {
+
   if (req.url === "/api/users" && req.method === "GET") {
     res.setHeader("Content-Type", "application/json");
     res.write(JSON.stringify(users));
     res.end();
+
   } else if (req.url.match(/\/api\/users\/[0-9]+/) && req.method === "GET") {
     const id = req.url.split("/")[3];
     const user = users.find((user) => user.id === parseInt(id));
     res.setHeader("Content-Type", "application/json");
+
     if (user) {
       res.write(JSON.stringify(user));
     } else {
@@ -24,7 +29,9 @@ const server = createServer((req, res) => {
       res.write(JSON.stringify({ message: "user not found" }));
     }
     res.end();
+    
   } else {
+    //for get request else
     res.setHeader("Content-Type", "application/json");
     res.statusCode = 404;
     res.write(JSON.stringify({ message: "Route not found" }));
