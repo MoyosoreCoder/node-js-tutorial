7ecff3c9d78e5654f25dded827497dfe73318577
Mariam <kelanimaryam1234@gmail.com>
2025-01-27 13:57:57 +0100

    more on file handling

--- filePath.js
+++ filePath.js
@@ -1,5 +1,16 @@
 import path from 'path'
 
 const filePath = './dir/dir2/test2.txt'
-
+//basename
 console.log(basename(filePath))
+
+//dirname
+console.log(path.dirname(filePath))
+
+//parse
+console.log(path.parse(filePath)); 
+
+//extname
+console.log(path.extname(filePath));
+
+
